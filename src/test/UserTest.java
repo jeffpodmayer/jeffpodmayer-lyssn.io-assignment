a86541be105f92673f652b5ee1887969327049cb
jeffpodmayer <jeff.podmayer@gmail.com>
2024-11-20 22:06:46 -0800

    WIP

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -1,11 +1,32 @@
 package test;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class UserTest {
 
     @BeforeEach
     void createUsers(){
+
+    }
+
+    @AfterEach
+    void deleteCreatedUsers(){
+
+    }
+
+    @Test
+    void testGetAllUsers(){
+
+    }
+
+    @Test
+    void testUpdateUser(){
+
+    }
+
+    void testGetUserByUserId(){
 
     }
 
