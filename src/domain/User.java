86d7ef166086a1167e117c83698bd72b7ba40592
jeffpodmayer <jeff.podmayer@gmail.com>
2024-11-20 21:56:33 -0800

    completed methods

--- src/domain/User.java
+++ src/domain/User.java
@@ -1,9 +1,6 @@
 package domain;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -12,14 +9,14 @@ public class User {
     Long userId;
     String fName;
     String name;
-    Timestamp signUpdate;
+    Timestamp signUpDate;
 
     // Constructor
-    public User(Integer userId, String fName, String name, Timestamp signUpdate) {
+    public User(Long userId, String fName, String name, Timestamp signUpDate) {
         this.userId = userId;
         this.fName = fName;
         this.name = name;
-        this.signUpdate = signUpdate;
+        this.signUpDate = signUpDate;
     }
 
     // Getters & Setters
@@ -48,39 +45,80 @@ public class User {
     }
 
     public Timestamp getSignUpdate() {
-        return signUpdate;
+        return signUpDate;
     }
 
     public void setSignUpdate(Timestamp signUpdate) {
-        this.signUpdate = signUpdate;
+        this.signUpDate = signUpDate;
     }
 
     @Override
     public String toString() {
-        return STR."User{userId=\{userId}, fName='\{fName}\{'\''}, name='\{name}\{'\''}, signUpdate=\{signUpdate}\{'}'}";
+        return STR."User{userId=\{userId}, fName='\{fName}\{'\''}, name='\{name}\{'\''}, signUpdate=\{signUpDate}\{'}'}";
     }
 
+   //Method to connect to a database
     private static Connection getConnection() throws SQLException {
         String url = "jdbc:postgresql://localhost:5432/mydb";
         return DriverManager.getConnection(url);
     }
 
-    public static Collection<User> getAllUsers(){
+    public static Collection<User> getAllUsers() throws SQLException {
         Collection<User> users = new ArrayList<>();
         String query = "SELECT * FROM Users";
+        try (Connection connection = getConnection();
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
 
+            while (rs.next()) {
+                long userid = rs.getLong("userid");
+                String fname = rs.getString("fname");
+                String name = rs.getString("name");
+                Timestamp signupdate = rs.getTimestamp("signupdate");
+                users.add(new User(userid, fname, name, signupdate));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return users;
     }
 
-    public static User updateUser(User user){
+    public static Boolean updateUser(User user) throws SQLException {
+        String updateQuery = "UPDATE users SET fname = ?, name = ?, signupdate = ? WHERE userid = ?";
+        try(Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)){
+            preparedStatement.setString(1, user.getfName());
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setTimestamp(3, user.getSignUpdate());
+            preparedStatement.setLong(4, user.getUserId());
 
-        return user;
+            int rowsUpdated = preparedStatement.executeUpdate();
+            return rowsUpdated > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public static User getUserByUserId(Long userId){
         String query = "SELECT * FROM users WHERE userid ?";
+        try(Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setLong(1,userId);
 
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                if (resultSet.next()) {
+                    Long userid = resultSet.getLong("userid");
+                    String fName = resultSet.getString("fName");
+                    String name = resultSet.getString("name");
+                    Timestamp signupdate = resultSet.getTimestamp("signupdate");
 
+                    return new User(userId, fName, name, signupdate);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
